4add6625594b983bb90b1ea7860b09f7b8611c4d
Alex <alexanderjukes@gmail.com>
2017-03-02 13:53:33 +0000

    Exercise 12: Loading file

--- directory_ex-12.rb
+++ directory_ex-12.rb
@@ -14,12 +14,35 @@ students = [
 {name: "Norman Bates", cohort: :november}
 ]
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+      @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+  end
+
 @students = []
 
 def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" #9 because we'll be adding more variables
 end
 
@@ -35,6 +58,10 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
